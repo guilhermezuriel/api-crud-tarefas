631ad66b0ca4bc81378b0063b9b25fa4a8a4ac61
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-01 19:37:47 -0300

    Buscando métodos em Routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http';
+import { json } from './middleware/json.js';
+import { routes } from './routes.js';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+  await json(req, res); //Espera carregar todos os dados do body e transforma-os de JSON para Object
+  const route = routes.find((route) => {
+    return route.method === method; //Observa se o método requisitado exige em routes.js
+  });
+  if (route) {
+    return route.handler(req, res);
+  }
+  return res.writeHead(404).end('Not Found');
 });
 
 server.listen(3335);
