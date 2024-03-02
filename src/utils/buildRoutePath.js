247150b9f17356b55884f32bb09c41afbcf40a4c
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-03-02 00:04:32 -0300

    Funções úteis na seleção de urls e de querys params

---
+++ src/utils/buildRoutePath.js
@@ -0,0 +1,11 @@
+export function buildRoutePath(path) {
+  const routeParamatersRegex = /:([a-zA-Z]+)/g; //Regex to Capture the :id
+  const pathWithParams = path.replaceAll(
+    routeParamatersRegex,
+    '(?<$1>[a-z0-9-_]+)',
+  ); //Replace the selected part, if there is one, for another Regex, to catch all the route path
+
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`); //Utilizes the pathRegex to get all the params, if there is one\
+
+  return pathRegex;
+}
